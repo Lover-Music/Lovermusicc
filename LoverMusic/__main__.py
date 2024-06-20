f381136c1b1d5df022b94b3cf5930d3f598638d2
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-06-21 02:24:16 +0530

    Update __main__.py

--- LoverMusic/__main__.py
+++ LoverMusic/__main__.py
@@ -1,18 +1,16 @@
 import asyncio
 import importlib
-import sys
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
 from LoverMusic import LOGGER, app, userbot
-from LoverMusic.core.call import Anon
+from LoverMusic.core.call import Anony
+from LoverMusic.misc import sudo
 from LoverMusic.plugins import ALL_MODULES
 from LoverMusic.utils.database import get_banned_users, get_gbanned
-
-loop = asyncio.get_event_loop()
+from config import BANNED_USERS
 
 
 async def init():
@@ -23,17 +21,9 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("LoverMusic").error(
-            "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
-        )
-        return
-    if (
-        not config.SPOTIFY_CLIENT_ID
-        and not config.SPOTIFY_CLIENT_SECRET
-    ):
-        LOGGER("LoverMusic").warning(
-            "Spotify Client Id & Secret not added, Chutiya Saala ek itni simple cheej nahi laa paaya."
-        )
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -45,28 +35,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("LoverMusic.plugins" + all_module)
-    LOGGER("LoverMusic.plugins").info(
-        "Necessary Modules Imported Successfully."
-    )
+        importlib.import_module("AnonXMusic.plugins" + all_module)
+    LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Anon.start()
+    await Anony.start()
     try:
-        await Anon.stream_call(
-            "https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4"
-        )
+        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("LoverMusic").error(
-            "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
+        LOGGER("AnonXMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
-        sys.exit()
+        exit()
     except:
         pass
-    await Anon.decorators()
-    LOGGER("LoverMusic").info("Music Bot Started Successfully, Now Gib your girlfriend chumt to @shubhamsah1")
+    await Anony.decorators()
+    LOGGER("AnonXMusic").info(
+        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
+    )
     await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
-    LOGGER("LoverMusic").info("Stopping Music Bot, Bhakk Bhosdike (Gaand Maraa Tu)")
+    asyncio.get_event_loop().run_until_complete(init())
