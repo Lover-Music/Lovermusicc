4317fd6a0c30c7a10ac7c961d982a3a9613256bb
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2024-06-20 14:10:35 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "14411702"))
 API_HASH = getenv("API_HASH", "6f8aaf5bd4f6538f06bc31ee67460f99")
 BOT_TOKEN = getenv("BOT_TOKEN", "5983601762:AAFDanO3qNTw4atWbxUYbWVzCpb64XOc6gU")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://userbot:userbot@cluster0.iweqz.mongodb.net/test?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001583255537"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "『˹𝑳𝒐𝒗𝒆𝒓 ✘ ℳ𝓾𝓼𝓲c‌˼』")
 OWNER_ID = list(
